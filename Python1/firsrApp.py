14581fbb716d8ce5347102a42b148c7e47bed7b9
Abulqasim <abulqasim.nazarov@gmail.com>
2025-01-27 16:45:31 +0400

    Complete If

---
+++ Python1/firsrApp.py
@@ -0,0 +1,25 @@
+# test-1 = 67 #kebab case
+# radiusOkrujnosti = 45  #Camel case
+# test_23 = 34 #snake case
+# DlinaKvadrata =87  #Pascal case
+
+
+
+# num1 = 45
+
+# print(num1==1)
+
+
+
+
+# if True:
+#     print("ok")
+# elif True:
+#     print("text")
+# elif True:
+#     print("pool")
+# else:
+#     print("not ok")
+
+
+
