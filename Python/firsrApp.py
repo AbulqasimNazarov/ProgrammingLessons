0cb22a7e1cbf5e5f802346e59e47518e612f9ccc
Abulqasim <abulqasim.nazarov@gmail.com>
2025-02-02 10:55:21 +0400

    Python Finish

---
+++ Python/firsrApp.py
@@ -0,0 +1,86 @@
+# test-1 = 67 #kebab case
+# radiusOkrujnosti = 45  #Camel case
+# test_23 = 34 #snake case
+# DlinaKvadrata =87  #Pascal case
+
+
+
+# num1 = 45
+
+# print(num1==1)
+
+
+
+
+# if True:
+#     print("ok")
+# elif True:
+#     print("text")
+# elif True:
+#     print("pool")
+# else:
+#     print("not ok")
+
+
+# import time
+
+
+# number = 25
+# while False:
+#     print(number)
+#     number += 1 # = number + 1
+#     time.sleep(1)
+
+
+
+
+
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 ** number2)
+# print(number1 / number2)
+# print(number1 // number2) # --4
+# print(number1 % number2)  # --1
+
+# num = 64
+# print(num // 2 > 32)
+
+
+# num = 25
+# i = 0
+
+# while i <= num:
+#     if i % 5 == 0:
+#         print(i)
+#     i += 1
+    
+    
+    
+# print("Enter number: ", end='')
+# a = int(input())
+# b = 55
+# total = 0
+
+# while a <= b:
+#     if a % 10 == 0:
+#         total += a
+#     a += 1
+    
+# print(total)
+
+
+# a = 8
+# i = 0
+
+# b = 5
+# j = 0
+
+# while j < b:
+#     while i < a:
+#         print("*", end='')
+#         i +=1
+#     i=0
+#     print(end='')
+#     print("*")
+#     j += 1
