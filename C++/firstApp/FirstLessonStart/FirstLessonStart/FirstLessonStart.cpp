72561cd12f84b7d32ac97aead7ffded86e9e57b3
Abulqasim <abulqasim.nazarov@gmail.com>
2025-02-02 12:23:02 +0400

    C++ get started

---
+++ C++/firstApp/FirstLessonStart/FirstLessonStart/FirstLessonStart.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //cout << "Hello World!" << endl;
+    //cout << "test";  
+    //int number = 0;
+    /*cout << "Enter number: ";
+    cin >> number;
+    cout << number;*/
+
+    //  My Name is ...
+    //i am age
+    /*int age = 13;
+    cout << "My Name Is Yaqub\n" << "i am " << age;*/
+    
+
+    /*bool cap = 0;
+    int num = 46;
+    double drob = 3.45;
+    string text = "hello";
+    char symbol1 = 65;
+    char symbol2 = 66;
+    cout << symbol1 << endl;*/
+
+    /*int num1 = 8;
+    int num2 = 5;
+    cout << num1 + num2 << endl;
+    cout << num1 - num2 << endl;
+    cout << num1 / num2 << endl;
+    cout << num1 * num2 << endl;
+    cout << (num1 % num2) << endl;*/
+    /*int number = 0;
+    cout << "Enter number: ";
+    cin >> number;*/
+    
+
+}
+
+
+
+
+
