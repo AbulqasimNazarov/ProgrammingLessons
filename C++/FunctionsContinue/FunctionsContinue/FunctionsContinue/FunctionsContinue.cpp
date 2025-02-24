b897af550f7ba88f079a40578cebfdeb141bd949
Abulqasim <abulqasim.nazarov@gmail.com>
2025-02-24 17:50:23 +0400

    Ternary operations

--- C++/FunctionsContinue/FunctionsContinue/FunctionsContinue/FunctionsContinue.cpp
+++ C++/FunctionsContinue/FunctionsContinue/FunctionsContinue/FunctionsContinue.cpp
@@ -17,12 +17,26 @@ void Minus(int num1, int num2) {
 
 
 int main() {
-    int num1 = 0, num2 = 0;
+    /*int num1 = 0, num2 = 0;
     cout << "Enter number1: ";
     cin >> num1;
     cout << "Enter number2: ";
     cin >> num2;
     Sum(num1, num2);
-    Minus(num1, num2);
+    Minus(num1, num2);*/
+
+
+
+    int height = 0;
+    cout << "Enter your triangle: ";
+    cin >> height;
+    for (int i = height; i > 0; i--)
+    {
+        for (int h = 0; h < i; h++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
 }
 
---
+++ C++/TernaryOperation/TernaryOperation/TernaryOperation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*char letter = 'A';
+	bool upRegister = true;
+	
+
+	string answer = upRegister ? "Upper" : "Lower";
+	cout << answer << endl;*/
+	/*if (upRegister)
+	{
+		cout << "Upper" << endl;
+	}
+	else {
+		cout << "Lower" << endl;
+	}*/
+
+
+
+
+	/*int num = 0;
+	cout << "Enter: ";
+	cin >> num;
+
+	bool isCorrectNum = true;*/
+
+	
+	/*if (num > 0)
+	{
+		cout << num << endl;
+	}
+	else {
+		while (num < 0)
+		{
+			cout << "Enter: ";
+			cin >> num;
+		}
+	}*/
+
+
+	int num = 0;
+	cin >> num;
+
+
+	bool test = num > 0 ? true : false;
+	cout << test << endl;
+
+}
+
