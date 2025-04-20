4d210d08a36e94f131ba091407fc303aa51a2383
Abulqasim <abulqasim.nazarov@gmail.com>
2025-04-20 12:34:18 +0400

    Template Functions end

---
+++ C++/Classwork/Classwork/Classwork.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+//void Cout(int num) {
+//	cout << num << endl;
+//}
+//
+//void Cout(double num) {
+//	cout << num << endl;
+//}
+//
+//void Cout() {
+//	cout << "hello";
+//}
+
+//template<typename T, typename T2>
+//void Cout(T num, T2 num2) {
+//	cout << num + num2<< endl;
+//}
+
+//template<typename T1, typename T2, typename TResult>
+//TResult Cout(T1 num, T2 num2) {
+//	TResult result = num + num2;
+//	return result;
+//}
+
+//template<typename T1, typename T2, typename maxValue>
+//maxValue Value(T1 num1, T2 num2) {
+//	if (num1 < num2) {
+//		return num1;
+//	}
+//	else {
+//		return num2;
+//	}
+//
+//}
+
+
+int main()
+{
+	/*double num_0 = 67;
+	int num_1 = 5;
+
+	double result = Value<double, int, double>(num_0, num_1);
+	cout << result << endl;*/
+
+
+
+
+
+	/*int num1 = 78;
+	double num2 = 78.5;
+
+	double test = Cout<double, int, double>(num2, num1);
+	cout << test << endl;*/
+
+
+
+
+	/*Cout(num2, num1);*/
+
+	/*Cout(num1);
+	Cout(num2);
+	Cout();*/
+
+
+
+
+
+	/*srand(time(0));
+	int test = rand() % (100-95)+95;
+	cout << test << endl;*/
+
+
+
+	/*srand(time(0));
+	int random = rand() % 10+1;
+	int user = 0;
+	while (user != random) {
+		cout << "Guess the number from 1-10: ";
+		cin >> user;
+		if (user < random) {
+			cout << "it is higher number" << endl;
+		}
+		else if (user > random) {
+			cout << "it is lower" << endl;
+		}
+		else {
+			cout << "you guessed correctly congrats" << endl;
+		}
+		
+	}*/
+	
+
+}
+
